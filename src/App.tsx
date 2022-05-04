42fcaff6cbffe7ca61cf8b8c3d477beedcc6816a
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-05-04 14:19:38 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,25 @@ const App = () => {
   const hasItems = items.length > 0
 
   return (
-    <main className="max-w-xl mt-8 mx-auto">
-      <div className="border border-gray-400 pt-6 px-4 pb-5">
-        <h1 className="text-3xl font-bold mb-5">Random item picker</h1>
+    <main className="max-w-xl mt-12 mx-auto text-center">
+      <div className="bg-white border border-gray-300 mx-4 py-7 px-6">
+        <h1 className="text-3xl font-bold mb-6">Random item picker</h1>
+
+        <p className="text-sm mb-4">
+          Add some items and then click "Pick random item".
+        </p>
 
         <AddBox />
 
         {hasItems && (
-          <ActionButton className="mt-1.5" onClick={clearItems}>
+          <ActionButton className="mt-1" onClick={clearItems}>
             Clear items
           </ActionButton>
         )}
 
         {items.length > 1 && (
           <button
-            className="btn block text-green-700 py-2 px-4 my-4 text-center
+            className="btn block text-green-700 py-2 px-4 mt-6 mb-4 text-center
               w-full border-2 border-green-700"
             onClick={pickRandomItem}
           >
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: [
+    "./public/index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
